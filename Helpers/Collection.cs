c31ee8e9c1aee5c9646815db6aaa6fd210d3b14c
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2025-05-22 16:56:06 +0300

    [TabsManager] Improve Helpres. Write SelectionCoordinator

---
+++ Helpers/Collection.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Windows.Input;
+using System.Windows;
+using System.Collections.Specialized;
+using System.Linq;
+using System;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TabsManagerExtension.Helpers {
+    public interface ISelectableGroup<TItem> {
+        ObservableCollection<TItem> SelectedItems { get; }
+    }
+
+
+    public class SelectionCoordinator<TGroup, TItem> where TGroup : ISelectableGroup<TItem> {
+        public Action<TGroup, TItem, bool>? OnItemSelectionChanged;
+
+        private readonly ObservableCollection<TGroup> _groups;
+        private bool _isSyncing;
+
+        public SelectionCoordinator(ObservableCollection<TGroup> groups) {
+            _groups = groups;
+
+            // Подписка на уже существующие
+            foreach (var group in _groups) {
+                SubscribeToGroup(group);
+            }
+
+            // Динамически обрабатываем добавление/удаление
+            _groups.CollectionChanged += OnGroupsChanged;
+        }
+
+        private void OnGroupsChanged(object sender, NotifyCollectionChangedEventArgs e) {
+            if (e.NewItems != null) {
+                foreach (TGroup group in e.NewItems) {
+                    SubscribeToGroup(group);
+                }
+            }
+
+            if (e.OldItems != null) {
+                foreach (TGroup group in e.OldItems) {
+                    UnsubscribeFromGroup(group);
+                }
+            }
+        }
+
+        private void SubscribeToGroup(TGroup group) {
+            // Отписываемся на всякий случай, даже если раньше не подписывались
+            group.SelectedItems.CollectionChanged -= HandleGroupSelectionChanged;
+            group.SelectedItems.CollectionChanged += HandleGroupSelectionChanged;
+        }
+        private void UnsubscribeFromGroup(TGroup group) {
+            group.SelectedItems.CollectionChanged -= HandleGroupSelectionChanged;
+        }
+
+        private void HandleGroupSelectionChanged(object sender, NotifyCollectionChangedEventArgs e) {
+            if (sender is not ObservableCollection<TItem> selectedItems)
+                return;
+
+            var group = _groups.FirstOrDefault(g => ReferenceEquals(g.SelectedItems, selectedItems));
+            if (group == null)
+                return;
+
+            if (e.Action == NotifyCollectionChangedAction.Add && e.NewItems != null) {
+                foreach (TItem item in e.NewItems) {
+                    OnItemSelectionChanged?.Invoke(group, item, true);
+                }
+            }
+            else if (e.Action == NotifyCollectionChangedAction.Remove && e.OldItems != null) {
+                foreach (TItem item in e.OldItems) {
+                    OnItemSelectionChanged?.Invoke(group, item, false);
+                }
+            }
+            else if (e.Action == NotifyCollectionChangedAction.Reset) {
+                // Reset не обрабатываем — мы сами отлавливаем очистку вручную при OnGroupSelectionChanged.
+            }
+
+            OnGroupSelectionChanged(group);
+        }
+
+
+
+        private void OnGroupSelectionChanged(TGroup changedGroup) {
+            // Защита от повторного входа, если метод уже выполняется
+            if (_isSyncing) return;
+
+            // Если зажат Ctrl — разрешаем множественный выбор, не сбрасываем другие группы
+            if ((Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
+                return;
+
+            // Устанавливаем флаг, чтобы предотвратить повторные вызовы из-за CollectionChanged
+            _isSyncing = true;
+
+            // Выполняем логику асинхронно в UI-диспетчере
+            // Это защищает от исключений и конфликтов в WPF/COM, например, при выборе вкладок
+            Application.Current.Dispatcher.InvokeAsync(() => {
+
+                // Проходим по всем группам, кроме той, где произошло текущее выделение
+                foreach (var group in _groups) {
+                    if (!ReferenceEquals(group, changedGroup)) {
+                        // Сохраняем текущее состояние выделения перед очисткой
+                        var removedItems = group.SelectedItems.ToList();
+
+                        // Снимаем выделение в этой группе
+                        group.SelectedItems.Clear(); // вызовет CollectionChanged (Reset)
+
+                        // Явно уведомляем, что эти элементы были сняты
+                        foreach (var item in removedItems) {
+                            OnItemSelectionChanged?.Invoke(group, item, false);
+                        }
+                    }
+                }
+                _isSyncing = false;
+            });
+        }
+
+        public IEnumerable<(TGroup Group, TItem Item)> GetAllSelectedItems() {
+            foreach (var group in _groups) {
+                foreach (var item in group.SelectedItems) {
+                    yield return (group, item);
+                }
+            }
+        }
+    }
+
+
+    namespace Ex {
+        public static class CollectionExtensions {
+            public static IEnumerable<(TGroup Group, TItem Item)> GetAllSelectedItems<TGroup, TItem>(
+                this IEnumerable<TGroup> groups) where TGroup : ISelectableGroup<TItem> {
+                foreach (var group in groups) {
+                    foreach (var item in group.SelectedItems) {
+                        yield return (group, item);
+                    }
+                }
+            }
+        }
+    }
+}
--- Helpers/Helpers.cs
+++ Helpers/Helpers.cs
@@ -7,6 +7,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
 using System.Windows;
+using System.Windows.Threading;
+using System.Collections.Specialized;
+using System.Windows.Controls;
+using Microsoft.VisualStudio.Shell;
 
 namespace TabsManagerExtension.Helpers {
     public class ObservableObject : INotifyPropertyChanged {
@@ -26,57 +30,49 @@ namespace TabsManagerExtension.Helpers {
         }
     }
 
-    public static class UI {
-        // Вспомогательный метод для поиска элементов Visual Tree (рекурсивно)
-        public static List<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject {
-            List<T> result = new List<T>();
 
-            if (depObj == null) return result;
-
-            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++) {
-                var child = VisualTreeHelper.GetChild(depObj, i);
-                if (child is T matchingChild) {
-                    result.Add(matchingChild);
-                }
-
-                // Рекурсивный вызов для вложенных детей
-                result.AddRange(FindVisualChildren<T>(child));
-            }
-
-            return result;
-        }
-
-        // Универсальный метод поиска родителя
-        public static T FindParent<T>(DependencyObject child) where T : DependencyObject {
-            while (child != null) {
-                if (child is T parent) {
-                    return parent;
-                }
-                child = VisualTreeHelper.GetParent(child);
-            }
-            return null;
+    public static class Time {
+        public static void RunWithDelay(TimeSpan delay, Action action) {
+            var timer = new DispatcherTimer { Interval = delay };
+            timer.Tick += (s, e) => {
+                timer.Stop();
+                action();
+            };
+            timer.Start();
         }
     }
-}
 
-namespace TabsManagerExtension.Helpers.Ex {
-    // TODO: move to extensions file.
-    public static class VisualTreeExtensions {
-        // Метод расширения для поиска всех потомков указанного типа (универсальный)
-        public static IEnumerable<T> GetVisualDescendants<T>(this DependencyObject parent) where T : DependencyObject {
-            if (parent == null) yield break;
+    //public static class UIDispatcher {
+    //    public static void Run(Action action) {
+    //        if (IsVsixEnvironment()) {
+    //            ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+    //            {
+    //                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+    //                action();
+    //            });
+    //        }
+    //        else {
+    //            Application.Current?.Dispatcher.InvokeAsync(action);
+    //        }
+    //    }
 
-            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++) {
-                var child = VisualTreeHelper.GetChild(parent, i);
+    //    public static Task RunAsync(Action action) {
+    //        if (IsVsixEnvironment()) {
+    //            return ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+    //            {
+    //                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+    //                action();
+    //            }).Task;
+    //        }
+    //        else {
+    //            return Application.Current?.Dispatcher.InvokeAsync(action).Task
+    //                   ?? Task.Run(action);
+    //        }
+    //    }
 
-                if (child is T typedChild) {
-                    yield return typedChild;
-                }
-
-                foreach (var descendant in GetVisualDescendants<T>(child)) {
-                    yield return descendant;
-                }
-            }
-        }
-    }
+    //    private static bool IsVsixEnvironment() {
+    //        // ThreadHelper works only in VS context, safe test:
+    //        return ThreadHelper.JoinableTaskFactory?.Context != null;
+    //    }
+    //}
 }
