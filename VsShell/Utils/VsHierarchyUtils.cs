c12bee1a291c37d6a08e61dc8dfb3edf55f45a5b
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2025-07-13 17:53:45 +0300

    {TabsManager] Improve / Refactor VsShell.Utils.

--- VsShell/Utils/VsHierarchyUtils.cs
+++ VsShell/Utils/VsHierarchyUtils.cs
@@ -12,12 +12,13 @@ using Microsoft.VisualStudio.TextManager.Interop;
 
 
 namespace TabsManagerExtension.VsShell.Utils {
-    public static class VsHierarchy {
+    public static class VsHierarchyUtils {
         public sealed class HierarchyItem {
             public IVsHierarchy Hierarchy { get; }
             public uint ItemId { get; }
             public string? Name { get; }
             public string? CanonicalName { get; }
+            public string? NormalizedPath { get; private set; }
 
             public HierarchyItem(IVsHierarchy hierarchy, uint itemId, string? name, string? canonicalName) {
                 this.Hierarchy = hierarchy;
@@ -26,6 +27,22 @@ namespace TabsManagerExtension.VsShell.Utils {
                 this.CanonicalName = canonicalName;
             }
 
+            public void CalculateNormilizedPath() {
+                var hierarchyItemName = this.CanonicalName ?? this.Name ?? string.Empty;
+                this.NormalizedPath = System.IO.Path.GetFullPath(hierarchyItemName)
+                    .TrimEnd(System.IO.Path.DirectorySeparatorChar, System.IO.Path.AltDirectorySeparatorChar);
+            }
+
+            public override bool Equals(object? obj) {
+                return obj is HierarchyItem other &&
+                       StringComparer.OrdinalIgnoreCase.Equals(this.CanonicalName, other.CanonicalName);
+            }
+
+            public override int GetHashCode() {
+                return this.CanonicalName != null
+                    ? StringComparer.OrdinalIgnoreCase.GetHashCode(this.CanonicalName)
+                    : 0;
+            }
             public override string ToString() {
                 return $"HierarchyItem(ItemId={this.ItemId}, Name='{this.Name}', CanonicalName='{this.CanonicalName}')";
             }
@@ -77,6 +94,44 @@ namespace TabsManagerExtension.VsShell.Utils {
         } // class Walker
 
 
+        //
+        // Api
+        //
+        public static void UnloadProject(Guid projectGuid) {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var vsSolution4 = (IVsSolution4)PackageServices.VsSolution;
+            vsSolution4.UnloadProject(ref projectGuid, (uint)_VSProjectUnloadStatus.UNLOADSTATUS_UnloadedByUser);
+
+            Helpers.Diagnostic.Logger.LogDebug($"[VsHierarchy] Unloaded project {projectGuid}");
+        }
+
+        public static void ReloadProject(Guid projectGuid) {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var vsSolution4 = (IVsSolution4)PackageServices.VsSolution;
+            vsSolution4.ReloadProject(ref projectGuid);
+
+            Helpers.Diagnostic.Logger.LogDebug($"[VsHierarchy] Reloaded project {projectGuid}");
+        }
+
+        
+        public static void UnloadProjects(IEnumerable<Guid> projectGuids) {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            foreach (var guid in projectGuids) {
+                VsHierarchyUtils.UnloadProject(guid);
+            }
+        }
+
+        public static void ReloadProjects(IEnumerable<Guid> projectGuids) {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            foreach (var guid in projectGuids) {
+                VsHierarchyUtils.ReloadProject(guid);
+            }
+        }
+
 
         public static List<HierarchyItem> CollectItemsRecursive(
             IVsHierarchy hierarchy,
@@ -86,8 +141,46 @@ namespace TabsManagerExtension.VsShell.Utils {
             ThreadHelper.ThrowIfNotOnUIThread();
 
             var result = new List<HierarchyItem>();
-            VsHierarchy.CollectItemsRecursiveInternal(hierarchy, itemId, predicate, result);
+            VsHierarchyUtils.CollectItemsRecursiveInternal(hierarchy, itemId, predicate, result);
             return result;
+        }
+
+
+        public static int ClickOnSolutionHierarchyItem(IVsHierarchy hierarchy, uint itemId) {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            // Используем ExecCommand для эмуляции DoubleClick в Solution Explorer,
+            // чтобы Visual Studio открыла файл так, как если бы пользователь дважды кликнул
+            // по нему именно в контексте этого проекта в папке External Dependencies.
+            if (hierarchy is IVsUIHierarchy uiHierarchy) {
+                Guid cmdGroup = VSConstants.CMDSETID.UIHierarchyWindowCommandSet_guid;
+                const uint cmdId = (uint)VSConstants.VsUIHierarchyWindowCmdIds.UIHWCMDID_DoubleClick;
+
+                hierarchy.GetProperty(itemId, (int)__VSHPROPID.VSHPROPID_Name, out var nameObj);
+                var name = nameObj as string ?? "(null)";
+                
+                hierarchy.GetCanonicalName(itemId, out var canonicalName);
+                Helpers.Diagnostic.Logger.LogDebug($"[ClickOnSolutionHierarchyItem] Try open ItemId={itemId}, Name='{name}' ({canonicalName})");
+
+                int hr = uiHierarchy.ExecCommand(
+                    itemId,
+                    ref cmdGroup,
+                    cmdId,
+                    0,
+                    IntPtr.Zero,
+                    IntPtr.Zero);
+
+                if (ErrorHandler.Succeeded(hr)) {
+                }
+                else {
+                    Helpers.Diagnostic.Logger.LogError($"[ClickOnSolutionHierarchyItem] Failed uiHierarchy.ExecCommand for itemId =  '{itemId}', hr=0x{hr:X8}");
+                }
+
+                return hr;
+            }
+
+            Helpers.Diagnostic.Logger.LogError($"[ClickOnSolutionHierarchyItem] Provided hierarchy does not implement IVsUIHierarchy.");
+            return VSConstants.E_FAIL;
         }
 
 
@@ -106,7 +199,7 @@ namespace TabsManagerExtension.VsShell.Utils {
                 string projectName = Utils.EnvDteUtils.GetDteProjectUniqueNameFromVsHierarchy(hierarchy);
                 Helpers.Diagnostic.Logger.LogDebug($"[Hierarchy] {projectName} (VSITEMID_ROOT)");
 
-                VsHierarchy.LogSolutionHierarchyRecursive(hierarchy, VSConstants.VSITEMID_ROOT, 0);
+                VsHierarchyUtils.LogSolutionHierarchyRecursive(hierarchy, VSConstants.VSITEMID_ROOT, 0);
             }
         }
 
@@ -133,7 +226,7 @@ namespace TabsManagerExtension.VsShell.Utils {
             }
 
             foreach (var childId in Walker.GetChildren(hierarchy, itemId)) {
-                VsHierarchy.CollectItemsRecursiveInternal(hierarchy, childId, predicate, result);
+                VsHierarchyUtils.CollectItemsRecursiveInternal(hierarchy, childId, predicate, result);
             }
         }
 
@@ -146,12 +239,24 @@ namespace TabsManagerExtension.VsShell.Utils {
 
                 hierarchy.GetCanonicalName(itemId, out var canonicalName);
 
+                string itemMarker = "";
+                
+                bool isSharedItem = false;
+                hierarchy.GetProperty(itemId,(int)__VSHPROPID7.VSHPROPID_IsSharedItem, out var isSharedItemObj);
+                if (isSharedItemObj is bool boolVal) {
+                    isSharedItem = boolVal;
+                }
+
+                if (isSharedItem) {
+                    itemMarker = " | Shared";
+                }
+
                 string indentStr = new string(' ', indent * 2);
-                Helpers.Diagnostic.Logger.LogDebug($"[{itemId}]{indentStr}{name} ({canonicalName})");
+                Helpers.Diagnostic.Logger.LogDebug($"[{itemId}]{indentStr}{name} ({canonicalName}){itemMarker}");
             }
 
             foreach (var childId in Walker.GetChildren(hierarchy, itemId)) {
-                VsHierarchy.LogSolutionHierarchyRecursive(hierarchy, childId, indent + 1);
+                VsHierarchyUtils.LogSolutionHierarchyRecursive(hierarchy, childId, indent + 1);
             }
         }
     }
