8213ef231f0127ad1f91087f232bef0c3afe56fe
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2025-07-13 17:52:29 +0300

    [TabsManager] Write TabsManagerService

--- ExtensionServices.cs
+++ ExtensionServices.cs
@@ -19,7 +19,9 @@ namespace TabsManagerExtension.Services {
     /// Потокобезопасная реализация singleton-сервиса с управляемым созданием экземпляра.
     /// Используется как базовый класс для IExtensionService-сервисов.
     /// </summary>
-    public abstract class SingletonServiceBase<T> where T : SingletonServiceBase<T>, IExtensionService, new() {
+    public abstract class SingletonServiceBase<T> 
+        where T : SingletonServiceBase<T>, IExtensionService, new() {
+
         private static readonly object _syncRoot = new();
         private static T? _instance;
 
@@ -79,6 +81,8 @@ namespace TabsManagerExtension.Services {
                 return;
             }
 
+            ExtensionServices.Register(Services.TimeManagerService.Create());
+
             ExtensionServices.Register(VsShell.Services.VsSelectionEventsServiceBase<
                 VsShell.Services.VsIDEStateFlagsTrackerService
                 >.Create());
@@ -95,7 +99,7 @@ namespace TabsManagerExtension.Services {
             ExtensionServices.Register(VsShell.Document.Services.VsDocumentActivationTrackerService.Create());
             ExtensionServices.Register(VsShell.Solution.Services.VsSolutionEventsTrackerService.Create());
             ExtensionServices.Register(VsShell.Solution.Services.SolutionHierarchyAnalyzerService.Create());
-            ExtensionServices.Register(VsShell.Solution.Services.ExternalDependenciesAnalyzerService.Create());
+            ExtensionServices.Register(VsShell.Solution.Services.IncludeDependencyAnalyzerService.Create());
             ExtensionServices.Register(VsShell.TextEditor.Services.TextEditorInputCommandFilterService.Create());
             ExtensionServices.Register(VsShell.TextEditor.Services.TextEditorFileNavigationCommandFilterService.Create());
 
---
+++ Package/TimeManagerService.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.VCProjectEngine;
+using Microsoft.VisualStudio.VCCodeModel;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Task = System.Threading.Tasks.Task;
+
+
+namespace TabsManagerExtension {
+    namespace Enums {
+        public enum TimerType {
+            _100ms,
+            _1s,
+            _3s,
+            _5s
+        }
+    }
+}
+
+
+namespace TabsManagerExtension.Services {
+    public class TimeManagerService :
+        SingletonServiceBase<TimeManagerService>,
+        IExtensionService {
+
+        private Helpers.Time.TimerManager<TimerTypeConfig, Enums.TimerType> _timerManager;
+
+        public TimeManagerService() { }
+
+        //
+        // IExtensionService
+        //
+        public IReadOnlyList<Type> DependsOn() {
+            return Array.Empty<Type>();
+        }
+
+
+        public void Initialize() {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            _timerManager = new Helpers.Time.TimerManager<TimerTypeConfig, Enums.TimerType>();
+            Helpers.Diagnostic.Logger.LogDebug("[TimeManagerService] Initialized.");
+        }
+
+
+        public void Shutdown() {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            ClearInstance();
+            Helpers.Diagnostic.Logger.LogDebug("[TimeManagerService] Disposed.");
+        }
+
+
+        //
+        // ░ API
+        // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+        //
+        public void Subscribe(Enums.TimerType type, Action handler) {
+            _timerManager.Subscribe(type, handler);
+        }
+
+        public void Unsubscribe(Enums.TimerType type, Action handler) {
+            _timerManager.Unsubscribe(type, handler);
+        }
+
+
+        //
+        // ░ Config
+        //
+        public class TimerTypeConfig : Helpers.Time.ITimerConfig<Enums.TimerType> {
+            public TimeSpan BaseInterval => TimeSpan.FromMilliseconds(100);
+
+            public Dictionary<Enums.TimerType, int> GetMultipliers() {
+                return new Dictionary<Enums.TimerType, int> {
+                    { Enums.TimerType._100ms, 1 },
+                    { Enums.TimerType._1s, 10 },
+                    { Enums.TimerType._3s, 30 },
+                    { Enums.TimerType._5s, 50 }
+                };
+            }
+        }
+    }
+}
